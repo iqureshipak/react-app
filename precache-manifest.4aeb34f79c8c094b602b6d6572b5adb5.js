91e8ab7d9dde432aacea3fbc425b67fd90a1ddbb
iqureshipak <i.qureshipak@gmail.com>
2019-06-23 23:35:06 +0500

    Updates

---
+++ precache-manifest.4aeb34f79c8c094b602b6d6572b5adb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461c1d7e60e60c726b268b33da4be8b9",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "0f6f4df8d57694eade63",
+    "url": "/react-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "27533a50b2272e1f0cfa",
+    "url": "/react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0f6f4df8d57694eade63",
+    "url": "/react-app/static/js/2.95533a2e.chunk.js"
+  },
+  {
+    "revision": "27533a50b2272e1f0cfa",
+    "url": "/react-app/static/js/main.ea497117.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
